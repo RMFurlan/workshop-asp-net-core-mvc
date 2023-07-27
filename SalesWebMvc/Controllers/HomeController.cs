384374b3e4d020b9a3f195a7c664562dfc023867
Rafael Medeiros Furlan <rafaelfurlan45@yahoo.com>
2023-07-27 10:56:37 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,18 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
